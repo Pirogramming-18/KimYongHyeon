09b48e1ad513981bae0bda386db2e8b023e0cc60
whereami0404 <whereami2048@gmail.com>
2023-01-03 20:12:47 +0900

    JavaScript-handling events

--- week02/Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ week02/Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -18,6 +18,26 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const target = event.target;
+  const key = dataset.key;
+  const value = dataset.value;
+  if (key == null || value == null) {
+    return;
+  }
+
+  const filtered = items.filter((item) => item[key] === value);
+  displayItems(filtered);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+
 // main
 loadItems()
   .then((items) => {
