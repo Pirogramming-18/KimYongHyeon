742a58915f386095f60d2fd9356ce089c9423284
whereami0404 <whereami2048@gmail.com>
2023-01-06 16:06:34 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,30 +1,40 @@
-num = 0
-cur = 1;
-while True:
-    try:
-        num = int(input("부를 숫자를 입력하세요(1,2,3만 입력 가능) : "))
-        if(num == 1 or num == 2 or num == 3):
-            break;
-        else:
-            print("1,2,3 중 하나를 입력하세요")
-    except ValueError:
-        print("정수를 입력하세요")
+cur = 0
+turn = True # true : playerA, false : playerB
+
+def selectNumber():
+    num = 0
+    while True:
+        try:
+            num = int(input("부를 숫자를 입력하세요(1,2,3만 입력 가능) : "))
+            if(num == 1 or num == 2 or num == 3):
+                break
+            else:
+                print("1,2,3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
+            
+    return num
+
+def printNumber(num, player):
+    global cur
+    for i in range(num):
+        cur += 1
+        if cur <= 31:
+            print(f"player{player} : ", cur)
         
-for i in range(num):
-    print("playerA : ", cur)
-    cur += 1
+        
+ #main 실행 코드       
+while True:
+    if turn: #playerA turn
+        num = selectNumber()
+        printNumber(num, "A")
+    else:
+        num = selectNumber()
+        printNumber(num, "B")
     
-while True:
-    try:
-        num = int(input("부를 숫자를 입력하세요(1,2,3만 입력 가능) : "))
-        if(num == 1 or num == 2 or num == 3):
-            break;
-        else:
-            print("1,2,3 중 하나를 입력하세요")
-    except ValueError:
-        print("정수를 입력하세요")
-        
-for i in range(num):
-    print("playerB : ", cur)
-    cur += 1
+    if(cur >= 31):
+        break
+    
+    turn = not turn
+
 
