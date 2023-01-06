5e623c6cf5c75db456bb05a05c6e5e98fcbc3fdd
whereami0404 <whereami2048@gmail.com>
2023-01-06 15:26:08 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -14,3 +14,17 @@ for i in range(num):
     print("playerA : ", cur)
     cur += 1
     
+while True:
+    try:
+        num = int(input("부를 숫자를 입력하세요(1,2,3만 입력 가능) : "))
+        if(num == 1 or num == 2 or num == 3):
+            break;
+        else:
+            print("1,2,3 중 하나를 입력하세요")
+    except ValueError:
+        print("정수를 입력하세요")
+        
+for i in range(num):
+    print("playerB : ", cur)
+    cur += 1
+
