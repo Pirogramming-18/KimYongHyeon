ab46a44746461305c282af8d87f2c855eb56f29c
whereami0404 <whereami2048@gmail.com>
2023-01-06 14:59:35 +0900

    1-2

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,1 +1,11 @@
 num = 0
+while True:
+    try:
+        num = int(input("부를 숫자를 입력하세요(1,2,3만 입력 가능) : "))
+        if(num == 1 or num == 2 or num == 3):
+            break;
+        else:
+            print("1,2,3 중 하나만 입력하세요")
+    except ValueError:
+        print("1,2,3 중 하나만 입력하세요")
+
